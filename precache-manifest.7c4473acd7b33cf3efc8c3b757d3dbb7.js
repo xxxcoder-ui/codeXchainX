f83834e62ec5e5d06e19b5b2824012e42f26dc80
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2020-10-30 05:49:53 -0400

    Initial commit

---
+++ precache-manifest.7c4473acd7b33cf3efc8c3b757d3dbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e7e3acd0fe5357f59c6beab292b1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3dcaa238da8f3799d9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6bfa2c644f3c6613be78",
+    "url": "/static/js/2.f030d0ca.chunk.js"
+  },
+  {
+    "revision": "fe3dcaa238da8f3799d9",
+    "url": "/static/js/main.6c1a0427.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
